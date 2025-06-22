0b726f39a83e1e05cc562e389a01322fdc99052b
Denis Pavlenko <denispavlenko7610new@gmail.com>
2025-06-22 16:32:44 +0300

    add pause menu

---
+++ Assets/Scripts/UI/UIInputHandler.cs
@@ -0,0 +1,15 @@
+﻿using TheLongNight.UI.PauseMenu;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class UIInputHandler : MonoBehaviour
+{
+    [SerializeField] private GamePause _gamePause;
+
+    public void OnPause(InputValue value)
+    {
+        if (value.isPressed)
+            _gamePause.TogglePause();
+    }
+}
