ae1ee7ca501d3ac391fee90d7e7ea63776b7dbf7
Denis Pavlenko <denispavlenko7610new@gmail.com>
2025-11-23 00:44:56 +0200

    del dotween, add rd_tween, add autoAssign attribute

---
+++ Assets/Utils/Assign/AutoAssignAttribute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assign
+{
+	using UnityEngine;
+
+	public class AutoAssignAttribute : PropertyAttribute
+	{
+		public readonly AutoAssignMode mode;
+
+		public AutoAssignAttribute(AutoAssignMode mode = AutoAssignMode.Local)
+		{
+			this.mode = mode;
+		}
+	}
+
+}
